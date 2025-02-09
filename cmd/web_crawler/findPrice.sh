b73b678e3c43bc9c89b0d7fd14bd9200d24d5a41
Mehran <Mehran.moradii@outlook.com>
2025-02-09 10:44:58 +0330

    Add home price to web_crawler.go

--- cmd/web_crawler/findPrice.sh
+++ cmd/web_crawler/findPrice.sh
@@ -3,3 +3,17 @@ Divar search example:
 
 2- for search and find all links: 
 curl "https://divar.ir/s/tehran/buy-residential/west-tehran-pars?size=65-80" | grep -i 'https://divar.ir/v/' | tr "," "\n"  | grep -i "url" | tr -d "\"" | cut -d ":" -f2-
+
+
+3- for line convert: 
+#!/bin/bash
+
+input_file="priceList.txt"  # Replace with the actual filename containing the URLs
+
+# Read the file and format the output
+formatted=$(awk '{printf "\"%s\", ", $0}' "$input_file")
+
+# Remove the trailing comma and space
+formatted=${formatted%, }
+
+echo "$formatted"
