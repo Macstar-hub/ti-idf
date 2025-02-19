0464c4c4ad4f9f43193301326b5b7d31a6cc37d2
root <root@macstarboy.emofid.com>
2025-02-19 13:44:06 +0330

    Add python venv and barchart function

--- cmd/jobs/price_analyzes/priceBarchart.py
+++ cmd/jobs/price_analyzes/priceBarchart.py
@@ -4,37 +4,46 @@ import mysql.connector
 
 # data from https://allisonhorst.github.io/palmerpenguins/
 
+mydb = mysql.connector.connect(
+host="127.0.0.1",
+user="root",
+password="test@test",
+database="words"
+)
+
+
 def historyBarPlot ():
+    prices, z_score = housePriceQuery("house_price")
+    print (prices, z_score)
+
     fruit_names = ['Coffee', 'Salted Caramel', 'Pistachio']
     fruit_counts = [4000, 2000, 7000]
 
     fig, ax = plt.subplots()
-    bar_container = ax.bar(fruit_names, fruit_counts)
-    ax.set(ylabel='pints sold', title='Gelato sales by flavor', ylim=(0, 8000))
+    bar_container = ax.bar(z_score, prices)
+    ax.set(xlabel='Z score Percent', ylabel='Per Squar Price', title='House Price Distribution')
     ax.bar_label(bar_container, fmt='{:,.0f}')
 
+    plt.savefig('./priceBarchart.png')
     plt.show() 
 
 
 
-def housePriceQuery(): 
-    mydb = mysql.connector.connect(
-    host="127.0.0.1",
-    user="root",
-    password="test@test",
-    database="words"
-    )
-
+def housePriceQuery(tableName): 
+    test = [1, 4, 10]
+    price = []
+    z_score_percent = []
     mycursor = mydb.cursor()
 
-    mycursor.execute("SELECT * FROM house_price")
+    mycursor.execute("SELECT per_squar, z_score FROM " + tableName)
 
     myresult = mycursor.fetchall()
 
     for x in myresult:
-        print(x)
+        price.append(x[0])
+        z_score_percent.append(x[1])
+        
 
+    return price, z_score_percent
 
-
-housePriceQuery()
 historyBarPlot()
