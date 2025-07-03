79a4dd7a1bf38927f2762704a817ab3c3d471da4
Mehran <Mehran.moradii@outlook.com>
2025-07-04 03:03:30 +0330

    Add minio object put and make bucket function

--- cmd/minio/minio.go
+++ cmd/minio/minio.go
@@ -3,7 +3,10 @@ package main
 import (
 	"context"
 	"log"
+	"os"
+	"time"
 
+	"github.com/cheggaaa/pb"
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
 )
@@ -28,24 +31,56 @@ func main() {
 	bucketName := "tutorial"
 	location := "us-east-1"
 
-	// Check bucket existing check:
+	createBucket(minioClient, ctx, bucketName, location, false)
+	objectName := "2025-07-02 09-34-47.mov"
+	filePath := "/Users/mehranmoradi/Desktop/2025-07-02 09-34-47.mov"
+	putObject(minioClient, ctx, bucketName, objectName, filePath, false)
+
+}
+
+func createBucket(minioClient *minio.Client, ctx context.Context, bucketName string, location string, debug bool) {
+	if debug {
+		minioClient.TraceOn(os.Stdout)
+	}
 	makeBucketError := minioClient.MakeBucket(ctx, bucketName, minio.MakeBucketOptions{Region: location, ForceCreate: true})
 	if makeBucketError != nil {
 		exists, errorBucketExist := minioClient.BucketExists(ctx, bucketName)
-		if errorBucketExist == nil && exists {
-			log.Printf("Already have bucket name: ", bucketName)
+		if errorBucketExist == nil && exists == true {
+			log.Printf("Already have bucket name: %s\n", bucketName)
 		} else {
 			log.Println("Cannot find bucket name: ", errorBucketExist)
 		}
 	}
-	objectName := "2025-07-02 09-34-47.mov"
-	filePath := "/Users/mehranmoradi/Desktop/2025-07-02 09-34-47.mov"
-	contentType := "application/octet-stream"
+
+	exists, errorBucketExist := minioClient.BucketExists(ctx, bucketName)
+	if errorBucketExist == nil && exists == true {
+		log.Printf("Already have bucket name: %s\n", bucketName)
+	}
+}
+
+func putObject(minioClient *minio.Client, ctx context.Context, bucketName string, objectName string, filePath string, debug bool) {
+	const contentType = "application/octet-stream"
+
+	if debug {
+		minioClient.TraceOn(os.Stdout)
+	}
+
+	fileInfo, err := os.Stat(filePath)
+	if err != nil {
+		log.Println("Cannot stats file with error: ", err)
+	}
+
+	// Make progress bar:
+	progress := pb.New64(fileInfo.Size())
+	progress.SetRefreshRate(50 * time.Microsecond)
+	progress.Start()
 
 	// Upload file:
 	info, uploadError := minioClient.FPutObject(ctx, bucketName, objectName, filePath, minio.PutObjectOptions{
 		ContentType: contentType,
+		Progress:    progress,
 	})
+
 	if uploadError != nil {
 		log.Println("Cannot upload a file with error: ", uploadError)
 	}
--- go.mod
+++ go.mod
@@ -3,14 +3,15 @@ module tf-idf
 go 1.23.3
 
 require (
+	github.com/cheggaaa/pb v1.0.29
 	github.com/gin-gonic/gin v1.10.0
 	github.com/go-sql-driver/mysql v1.8.1
 	github.com/gregscott94/z-table-golang v1.0.0
 	github.com/labstack/gommon v0.4.2
 	github.com/mavihq/persian v0.0.0-20231020110200-3e779b10be51
+	github.com/minio/minio-go/v7 v7.0.94
 	github.com/montanaflynn/stats v0.7.1
 	github.com/redis/go-redis/v9 v9.7.3
-	github.com/robfig/cron v1.2.0
 	gonum.org/v1/plot v0.14.0
 )
 
@@ -26,6 +27,7 @@ require (
 	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/fatih/color v1.18.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-fonts/liberation v0.3.2 // indirect
@@ -42,16 +44,17 @@ require (
 	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/minio/crc64nvme v1.0.1 // indirect
 	github.com/minio/md5-simd v1.1.2 // indirect
-	github.com/minio/minio-go/v7 v7.0.94 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/philhofer/fwd v1.1.3-0.20240916144458-20a13a1f6b7c // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/rs/xid v1.6.0 // indirect
 	github.com/tinylib/msgp v1.3.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
