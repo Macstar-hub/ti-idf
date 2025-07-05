f08fd24c3ceb5fa34379089122f7120fe0bb8479
Mehran <Mehran.moradii@outlook.com>
2025-07-05 21:33:54 +0330

    Update add upload file to landing page.

--- main.go
+++ main.go
@@ -167,6 +167,7 @@ func LandigPage() {
 	server.LoadHTMLGlob("./web/**/*")
 	server.StaticFile("/", "./web/landingPages/linkSubmit.html")
 	server.StaticFile("/assetcalc", "./web/landingPages/assetCalc.html")
+	server.StaticFile("/uploadfile", "./web/landingPages/upload.html")
 	server.POST("/api/v1/postLinks", httppost.PostLabels)
 	server.POST("/api/v1/assetcalc", httppost.CalcAsset)
 	server.GET("/api/v1/linkslist", httppost.ShowLinks)
